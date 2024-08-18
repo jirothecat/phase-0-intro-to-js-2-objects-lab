193f9f97763dcfb5ac2c04d1bfae0dea374fbcfc
Dylan Tin <dylant711@gmail.com>
2024-08-18 16:26:01 -0400

    completed objects lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: "Donald Trump",
+    streetAddress: "1234 White House Rd"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = { ...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
